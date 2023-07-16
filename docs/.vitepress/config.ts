d8a22f6042e144d6ea94b24bb06179c13324754d
Max Nowack <maxnowack@users.noreply.github.com>
2023-07-16 22:22:22 +0200

    docs: add favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -83,5 +83,15 @@ export default defineConfig({
     search: {
       provider: 'local',
     },
-  }
+  },
+
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#0367e9' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#0367e9' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
 })
