3814adf7ba51bfb7db86445ddb59a98ba7ba4b83
Max Nowack <max.nowack@gmail.com>
2023-07-16 08:23:47 +0200

    docs: fix base path for docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "SignalDB",
   description: "Documentation",
+  base: '/signaldb/',
 
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -77,6 +78,10 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/maxnowack/signaldb' }
-    ]
+    ],
+
+    search: {
+      provider: 'local',
+    },
   }
 })
