327ed0d7c5597cfb0c27d1eb5347682dad55ac8e
Max Nowack <maxnowack@users.noreply.github.com>
2023-07-17 10:39:42 +0200

    chore: build bundle with unbuild

--- src/persistence/createFilesystemAdapter.ts
+++ src/persistence/createFilesystemAdapter.ts
@@ -1,5 +1,12 @@
-import fs from 'fs'
+import type fsType from 'fs'
 import createPersistenceAdapter from './createPersistenceAdapter'
+
+// weird workaround to make the unbuild bundle work in the browser
+const loadFsModule = async () => {
+  const fsModule = 'fs'
+  const fs: typeof fsType = await import(fsModule)
+  return fs
+}
 
 export default function createFilesystemAdapter<
   T extends { id: I } & Record<string, any>,
@@ -7,6 +14,8 @@ export default function createFilesystemAdapter<
 >(filename: string) {
   return createPersistenceAdapter<T, I>({
     async register(onChange) {
+      if (typeof window !== 'undefined') throw new Error('Filesystem adapter is not supported in the browser')
+      const fs = await loadFsModule()
       const exists = await fs.promises.access(filename).then(() => true).catch(() => false)
       if (!exists) await fs.promises.writeFile(filename, '[]')
       fs.watch(filename, { encoding: 'utf8' }, () => {
@@ -14,6 +23,8 @@ export default function createFilesystemAdapter<
       })
     },
     async load() {
+      if (typeof window !== 'undefined') throw new Error('Filesystem adapter is not supported in the browser')
+      const fs = await loadFsModule()
       const exists = await fs.promises.access(filename).then(() => true).catch(() => false)
       if (!exists) return { items: [] }
       const contents = await fs.promises.readFile(filename, 'utf8')
@@ -21,6 +32,8 @@ export default function createFilesystemAdapter<
       return { items }
     },
     async save(items) {
+      if (typeof window !== 'undefined') throw new Error('Filesystem adapter is not supported in the browser')
+      const fs = await loadFsModule()
       await fs.promises.writeFile(filename, JSON.stringify(items))
     },
   })
