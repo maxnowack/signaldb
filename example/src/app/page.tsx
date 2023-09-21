aa4d3465c32f5d92e761a41c0cc6a1feee2173b7
Max Nowack <maxnowack@users.noreply.github.com>
2023-09-21 14:56:11 +0200

    fix: hydration errors in example

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import App from '../containers/App'
+import dynamic from 'next/dynamic'
+
+const App = dynamic(() => import('../containers/App'), { ssr: false })
 
 export default function Home() {
   return <App />
