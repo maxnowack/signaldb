6e10b1e021f1ca5b2048836a850d95bb14f01d89
Max Nowack <maxnowack@users.noreply.github.com>
2024-04-03 21:09:12 +0200

    docs: add http replication example

---
+++ examples/replication-http/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/examples/replication-http',
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: config => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+      },
+    },
+  }),
+}
+
+module.exports = nextConfig
