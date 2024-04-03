e4a9e09751bc25ecb62e6d78157a2221fba45db0
Max Nowack <maxnowack@users.noreply.github.com>
2024-04-03 21:21:21 +0200

    docs: cache data in local storage in BaaS examples

--- examples/supabase/src/models/Todos.ts
+++ examples/supabase/src/models/Todos.ts
@@ -1,15 +1,18 @@
-import { Collection } from 'signaldb'
+import { Collection, combinePersistenceAdapters, createLocalStorageAdapter } from 'signaldb'
 import maverickjsReactivityAdapter from 'signaldb-plugin-maverickjs'
 import createSupabasePersistenceAdapter from '../utils/createSupabasePersistenceAdapter'
 
 const Todos = new Collection<{ id: string, text: string, completed: boolean }>({
   memory: [],
   reactivity: maverickjsReactivityAdapter,
-  persistence: createSupabasePersistenceAdapter<{
-    id: string,
-    text: string,
-    completed: boolean,
-  }, string>('todos'),
+  persistence: combinePersistenceAdapters(
+    createLocalStorageAdapter('todos'),
+    createSupabasePersistenceAdapter<{
+      id: string,
+      text: string,
+      completed: boolean,
+    }, string>('todos'),
+  ),
 })
 Todos.on('persistence.error', (error) => {
   // eslint-disable-next-line no-console
