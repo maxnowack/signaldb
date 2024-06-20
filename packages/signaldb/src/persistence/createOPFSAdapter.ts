17ae799bf99948061838ea4866910ca554a690cb
James Gibson <sparhawk14@gmail.com>
2024-06-19 23:04:27 -0600

    feat: add OPFS persistence adapter
    delint and export
    
    add opfs tests
    
    test: implement mocking of navigator.storage
    
    passing tests; create file on save.

---
+++ packages/signaldb/src/persistence/createOPFSAdapter.ts
@@ -0,0 +1,70 @@
+import createPersistenceAdapter from './createPersistenceAdapter'
+
+export default function createOPFSAdapter<
+  T extends { id: I } & Record<string, any>,
+  I,
+>(filename: string) {
+  let savePromise: Promise<void> | null = null
+
+  async function getItems(): Promise<T[]> {
+    const opfsRoot = await navigator.storage.getDirectory()
+    const existingFileHandle = await opfsRoot.getFileHandle(filename, { create: true })
+    const contents = await existingFileHandle.getFile().then(val => val.text())
+    return JSON.parse(contents || '[]')
+  }
+
+  return createPersistenceAdapter<T, I>({
+    async register(onChange) {
+      const opfsRoot = await navigator.storage.getDirectory()
+      await opfsRoot.getFileHandle(filename, { create: true })
+      void onChange()
+    },
+    async load() {
+      if (savePromise) await savePromise
+
+      const items = await getItems()
+      return { items }
+    },
+    async save(_items, { added, modified, removed }) {
+      if (savePromise) await savePromise
+      const opfsRoot = await navigator.storage.getDirectory()
+      const existingFileHandle = await opfsRoot.getFileHandle(filename, { create: true })
+      if (added.length === 0 && modified.length === 0 && removed.length === 0) {
+        const writeStream = await existingFileHandle.createWritable()
+        await writeStream.write(JSON.stringify(_items))
+        await writeStream.close()
+        await savePromise
+        return
+      }
+      savePromise = getItems()
+        .then((currentItems) => {
+          const items = currentItems.slice()
+          added.forEach((item) => {
+            items.push(item)
+          })
+          modified.forEach((item) => {
+            const index = items.findIndex(({ id }) => id === item.id)
+            /* istanbul ignore if -- @preserve */
+            if (index === -1) throw new Error(`Item with ID ${item.id as string} not found`)
+            items[index] = item
+          })
+          removed.forEach((item) => {
+            const index = items.findIndex(({ id }) => id === item.id)
+            /* istanbul ignore if -- @preserve */
+            if (index === -1) throw new Error(`Item with ID ${item.id as string} not found`)
+            items.splice(index, 1)
+          })
+          return items
+        })
+        .then(async (items) => {
+          const writeStream = await existingFileHandle.createWritable()
+          await writeStream.write(JSON.stringify(items))
+          await writeStream.close()
+        })
+        .then(() => {
+          savePromise = null
+        })
+      await savePromise
+    },
+  })
+}
