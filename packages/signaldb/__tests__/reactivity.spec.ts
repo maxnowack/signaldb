7825e09f3e7208f2e4d5eb532c16263e84870858
Max Nowack <maxnowack@users.noreply.github.com>
2024-07-29 16:46:04 +0200

    test: add tests for primitive reactivity

---
+++ packages/signaldb/__tests__/reactivity.spec.ts
@@ -0,0 +1,168 @@
+import { vi, describe, it, expect } from 'vitest'
+import { Collection, createReactivityAdapter } from '../src'
+import deepClone from '../src/utils/deepClone'
+
+const primitiveReactivity = (() => {
+  class Computation {
+    effectCallback: () => void
+    onInvalidateCallbacks: (() => void)[] = []
+    constructor(effectCallback: () => void) {
+      this.effectCallback = effectCallback
+    }
+
+    onInvalidate(callback: () => void) {
+      this.onInvalidateCallbacks.push(callback)
+    }
+
+    invalidate() {
+      this.onInvalidateCallbacks.forEach(callback => callback())
+    }
+  }
+
+  let currentComputation: Computation | null = null
+  let lastComputation: Computation | null = null
+
+  function signal<T>(initialValue: T) {
+    let value = initialValue
+    const computationDeps = new Set<Computation>()
+    const signalValue = () => {
+      if (currentComputation) computationDeps.add(currentComputation)
+      return value
+    }
+    signalValue.set = (newValue: T) => {
+      value = newValue
+      computationDeps.forEach(computation => computation.effectCallback())
+    }
+    return signalValue
+  }
+  const effect = (callback: () => void) => {
+    const effectCallback = () => {
+      if (lastComputation) lastComputation.invalidate()
+      currentComputation = new Computation(effectCallback)
+      lastComputation = currentComputation
+      callback()
+      currentComputation = null
+    }
+    effectCallback()
+  }
+  function peek<T>(callback: () => T) {
+    const previousComputation = currentComputation
+    currentComputation = null
+    const result = callback()
+    currentComputation = previousComputation
+    return result
+  }
+  return {
+    effect,
+    signal,
+    peek,
+    getCurrentComputation: () => currentComputation,
+  }
+})()
+const primitiveReactivityAdapter = createReactivityAdapter({
+  create: () => {
+    const dep = primitiveReactivity.signal(0)
+    return {
+      depend: () => {
+        dep()
+      },
+      notify: () => {
+        dep.set(primitiveReactivity.peek(() => dep() + 1))
+      },
+    }
+  },
+  onDispose: (dispose) => {
+    primitiveReactivity.getCurrentComputation()?.onInvalidate(dispose)
+  },
+  isInScope: () => !!primitiveReactivity.getCurrentComputation(),
+})
+
+const tick = () => new Promise((resolve) => { setTimeout(resolve, 0) })
+
+describe('reactivity primitives', () => {
+  it('should be reactive with primitive adapter', async () => {
+    const collection = new Collection({ reactivity: primitiveReactivityAdapter })
+    const callback = vi.fn()
+
+    primitiveReactivity.effect(() => {
+      callback(collection.find({ name: 'John' }).count())
+    })
+    expect(callback).toHaveBeenCalledTimes(1)
+    expect(callback).toHaveBeenLastCalledWith(0)
+
+    await tick()
+    collection.insert({ id: '1', name: 'John' })
+    await tick()
+
+    expect(callback).toHaveBeenCalledTimes(2)
+    expect(callback).toHaveBeenLastCalledWith(1)
+  })
+
+  it('should call dispose on cleanup', async () => {
+    const reactivity = primitiveReactivityAdapter
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unnecessary-type-assertion
+    reactivity.onDispose = vi.fn(reactivity.onDispose!)
+
+    const collection = new Collection({ reactivity })
+
+    primitiveReactivity.effect(() => {
+      collection.find({ name: 'John' }).count()
+    })
+    expect(reactivity.onDispose).toHaveBeenCalledTimes(1)
+    await tick()
+
+    collection.insert({ id: '1', name: 'John' })
+    await tick()
+    expect(reactivity.onDispose).toHaveBeenCalledTimes(2)
+  })
+
+  it('should be reactive on updates', async () => {
+    const collection = new Collection({ reactivity: primitiveReactivityAdapter })
+    const callback = vi.fn()
+
+    primitiveReactivity.effect(() => {
+      const items = collection.find({ name: 'John' }).fetch()
+      callback(items)
+    })
+    await tick()
+
+    collection.insert({ id: '1', name: 'John' })
+    await tick()
+    expect(callback).toHaveBeenCalledTimes(2)
+    expect(callback).toHaveBeenLastCalledWith([{ id: '1', name: 'John' }])
+
+    collection.updateOne({ id: '1' }, { $set: { updated: true } })
+    await tick()
+
+    expect(callback).toHaveBeenCalledTimes(3)
+    expect(callback).toHaveBeenLastCalledWith([{ id: '1', name: 'John', updated: true }])
+
+    collection.updateOne({ id: '1' }, { $set: { updated: 2 } })
+    await tick()
+
+    expect(callback).toHaveBeenCalledTimes(4)
+    expect(callback).toHaveBeenLastCalledWith([{ id: '1', name: 'John', updated: 2 }])
+  })
+
+  it('should be reactive on dot notation fields updates', async () => {
+    const collection = new Collection({ reactivity: primitiveReactivityAdapter })
+    const callback = vi.fn()
+
+    primitiveReactivity.effect(() => {
+      const items = collection.find({ name: 'John' }).fetch()
+      callback(deepClone(items))
+    })
+    collection.insert({ id: '1', name: 'John', count: { a: 1 } })
+    await tick()
+    expect(callback).toHaveBeenCalledTimes(2)
+    expect(callback).toHaveBeenLastCalledWith([{ id: '1', name: 'John', count: { a: 1 } }])
+
+    collection.updateOne({ name: 'John' }, {
+      $set: { 'count.a': 2 },
+    })
+    await tick()
+
+    expect(callback).toHaveBeenCalledTimes(3)
+    expect(callback).toHaveBeenLastCalledWith([{ id: '1', name: 'John', count: { a: 2 } }])
+  })
+})
