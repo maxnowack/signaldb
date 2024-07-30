57a284d9b9fe0aac93ec140799ccaf074fff10b8
Max Nowack <maxnowack@users.noreply.github.com>
2024-07-30 13:47:17 +0200

    chore(deps): upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:vitest/recommended',
+    'plugin:vitest/legacy-recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
