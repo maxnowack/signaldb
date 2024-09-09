87d0d3f1f5fa6e08be89ed9beaaadc347548569f
Max Nowack <maxnowack@users.noreply.github.com>
2024-09-09 21:49:11 +0200

    docs: add jsdoc comments to sync manager

--- packages/signaldb/src/SyncManager/computeChanges.ts
+++ packages/signaldb/src/SyncManager/computeChanges.ts
@@ -1,5 +1,11 @@
 import isEqual from '../utils/isEqual'
 
+/**
+ * Compute changes between two arrays of items.
+ * @param oldItems Array of the old items
+ * @param newItems Array of the new items
+ * @returns The changeset
+ */
 export default function computeChanges<T extends Record<string, any>>(
   oldItems: T[],
   newItems: T[],
--- packages/signaldb/src/SyncManager/getSnapshot.ts
+++ packages/signaldb/src/SyncManager/getSnapshot.ts
@@ -1,6 +1,12 @@
 import type { BaseItem } from '../Collection'
 import type { LoadResponse } from '../types/PersistenceAdapter'
 
+/**
+ * Gets the snapshot of items from the last snapshot and the changes.
+ * @param lastSnapshot The last snapshot of items
+ * @param data The changes to apply to the last snapshot
+ * @returns The new snapshot of items
+ */
 export default function getSnapshot<ItemType extends BaseItem<IdType>, IdType>(
   lastSnapshot: ItemType[] | undefined,
   data: LoadResponse<ItemType>,
