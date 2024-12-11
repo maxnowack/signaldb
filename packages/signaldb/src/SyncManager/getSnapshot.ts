9440d71d5edc7154533a693937bd4c302d0ff7f8
Max Nowack <maxnowack@users.noreply.github.com>
2024-12-11 16:03:27 +0100

    fix: upsert data when applying to snapshot

--- packages/signaldb/src/SyncManager/getSnapshot.ts
+++ packages/signaldb/src/SyncManager/getSnapshot.ts
@@ -14,10 +14,21 @@ export default function getSnapshot<ItemType extends BaseItem<IdType>, IdType>(
   if (data.items != null) return data.items
 
   const items = lastSnapshot || []
-  data.changes.added.forEach(item => items.push(item))
+  data.changes.added.forEach((item) => {
+    const index = items.findIndex(i => i.id === item.id)
+    if (index !== -1) {
+      items[index] = item
+    } else {
+      items.push(item)
+    }
+  })
   data.changes.modified.forEach((item) => {
     const index = items.findIndex(i => i.id === item.id)
-    if (index !== -1) items[index] = item
+    if (index !== -1) {
+      items[index] = item
+    } else {
+      items.push(item)
+    }
   })
   data.changes.removed.forEach((item) => {
     const index = items.findIndex(i => i.id === item.id)
