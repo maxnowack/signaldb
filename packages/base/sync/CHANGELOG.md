e88d66653202a8cf3a773a9f58c7571055880c4c
Max Nowack <maxnowack@users.noreply.github.com>
2025-01-10 13:16:18 +0100

    fix: dispose collections in applyChanges

--- packages/base/sync/CHANGELOG.md
+++ packages/base/sync/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 * Define names for internal collections
+* Dispose temporary collections in applyChanges function
 
 ## [1.0.0] - 2024-12-16
 
--- packages/base/sync/__tests__/applyChanges.spec.ts
+++ packages/base/sync/__tests__/applyChanges.spec.ts
@@ -18,40 +18,40 @@ function getDefaultChangeItem(id = '1') {
   return { id, time: Date.now(), collectionName: 'test' }
 }
 
-it('should insert new items', () => {
+it('should insert new items', async () => {
   const items: TestItem[] = [{ id: 1, name: 'Item 1' }]
   const changes: Change<TestItem, number>[] = [
     { ...getDefaultChangeItem(), type: 'insert', data: { id: 2, name: 'Item 2' } },
   ]
 
-  const result = applyChanges(items, changes)
+  const result = await applyChanges(items, changes)
   expect(result).toEqual([
     { id: 1, name: 'Item 1' },
     { id: 2, name: 'Item 2' },
   ])
 })
 
-it('should update an item', () => {
+it('should update an item', async () => {
   const items: TestItem[] = [{ id: 1, name: 'Item 1' }]
   const changes: Change<TestItem, number>[] = [
     { ...getDefaultChangeItem(), type: 'update', data: { id: 1, modifier: { $set: { name: 'Updated Item 1' } } } },
   ]
 
-  const result = applyChanges(items, changes)
+  const result = await applyChanges(items, changes)
   expect(result).toEqual([{ id: 1, name: 'Updated Item 1' }])
 })
 
-it('should remove an item', () => {
+it('should remove an item', async () => {
   const items: TestItem[] = [{ id: 1, name: 'Item 1' }, { id: 2, name: 'Item 2' }]
   const changes: Change<TestItem, number>[] = [
     { ...getDefaultChangeItem(), type: 'remove', data: 1 },
   ]
 
-  const result = applyChanges(items, changes)
+  const result = await applyChanges(items, changes)
   expect(result).toEqual([{ id: 2, name: 'Item 2' }])
 })
 
-it('should handle a mix of inserts, updates, and removes', () => {
+it('should handle a mix of inserts, updates, and removes', async () => {
   const items: TestItem[] = [{ id: 1, name: 'Item 1' }]
   const changes: Change<TestItem, number>[] = [
     { ...getDefaultChangeItem('1'), type: 'insert', data: { id: 2, name: 'Item 2' } },
@@ -59,28 +59,28 @@ it('should handle a mix of inserts, updates, and removes', () => {
     { ...getDefaultChangeItem('3'), type: 'remove', data: 2 },
   ]
 
-  const result = applyChanges(items, changes)
+  const result = await applyChanges(items, changes)
   expect(result).toEqual([{ id: 1, name: 'Updated Item 1' }])
 })
 
-it('should update already existing items with insert', () => {
+it('should update already existing items with insert', async () => {
   const items: TestItem[] = [{ id: 1, name: 'Item 1' }]
   const changes: Change<TestItem, number>[] = [
     { ...getDefaultChangeItem(), type: 'insert', data: { id: 1, name: 'New Item Name' } },
   ]
 
-  const result = applyChanges(items, changes)
+  const result = await applyChanges(items, changes)
   expect(result).toEqual([
     { id: 1, name: 'New Item Name' },
   ])
 })
 
-it('should insert non-existing items on update', () => {
+it('should insert non-existing items on update', async () => {
   const items: TestItem[] = []
   const changes: Change<TestItem, number>[] = [
     { ...getDefaultChangeItem(), type: 'update', data: { id: 1, modifier: { $set: { name: 'Updated Item 1' } } } },
   ]
 
-  const result = applyChanges(items, changes)
+  const result = await applyChanges(items, changes)
   expect(result).toEqual([{ id: 1, name: 'Updated Item 1' }])
 })
