fd9188bd53397ca941a9716715de4782393740a9
Karba <ivan.karbashevskyi@gmail.com>
2025-02-07 14:38:59 +0100

    test: Add tests for new parameters in updateOne and updateMany methods in Collection.spec.ts and cover modify function with tests for custom options and array filters in modify.spec.ts

---
+++ packages/base/core/__tests__/modify.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from 'vitest'
+import modify from '../src/utils/modify'
+
+describe('modify', () => {
+  it('should apply modifier with default options', () => {
+    const item = { a: 1, b: 2 }
+    const modifier = { $set: { b: 3, c: 4 } }
+    const result = modify(item, modifier)
+    expect(result).toEqual({ a: 1, b: 3, c: 4 })
+  })
+
+  it('should apply modifier with custom options', () => {
+    const item = { a: 1, b: 2 }
+    const modifier = { $set: { b: 3, c: 4 } }
+    const options = { upsert: true }
+    const result = modify(item, modifier, [], {}, options)
+    expect(result).toEqual({ a: 1, b: 3, c: 4 })
+  })
+
+  it('should apply modifier with array filters', () => {
+    const item = { a: 1, b: [{ x: 1 }, { x: 2 }] }
+    const modifier = { $set: { 'b.$[elem].x': 3 } }
+    const arrayFilters = [{ 'elem.x': 2 }]
+    const result = modify(item, modifier, arrayFilters)
+    expect(result).toEqual({ a: 1, b: [{ x: 1 }, { x: 3 }] })
+  })
+})
