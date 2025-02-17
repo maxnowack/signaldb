ac8f4860075820fe77ba5cc73b0c366d1b1a0881
Max Nowack <maxnowack@users.noreply.github.com>
2025-02-17 12:11:29 +0100

    fix: type checking for query selectors

--- packages/base/core/src/types/Selector.ts
+++ packages/base/core/src/types/Selector.ts
@@ -1,90 +1,87 @@
 // original from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/meteor/mongo.d.ts
 
 export interface FieldExpression<T> {
-  $eq?: T | undefined,
-  $gt?: T | undefined,
-  $gte?: T | undefined,
-  $lt?: T | undefined,
-  $lte?: T | undefined,
+  $eq?: T,
+  $gt?: T,
+  $gte?: T,
+  $lt?: T,
+  $lte?: T,
   $in?: T[] | undefined,
-  $nin?: T[] | undefined,
-  $ne?: T | undefined,
-  $exists?: boolean | undefined,
-  $not?: FieldExpression<T> | undefined,
-  $expr?: FieldExpression<T> | undefined,
+  $nin?: T[],
+  $ne?: T,
+  $exists?: boolean,
+  $not?: FieldExpression<T>,
+  $expr?: FieldExpression<T>,
   $jsonSchema?: any,
-  $mod?: number[] | undefined,
-  $regex?: RegExp | string | undefined,
-  $options?: string | undefined,
-  $text?:
-    | {
-      $search: string,
-      $language?: string | undefined,
-      $caseSensitive?: boolean | undefined,
-      $diacriticSensitive?: boolean | undefined,
-    }
-    | undefined,
-  $where?: string | ((item: T) => boolean) | undefined,
-  $all?: T[] | undefined,
-
-  $elemMatch?: T extends object ? Query<T> : FieldExpression<T> | undefined,
-  $size?: number | undefined,
+  $mod?: number[],
+  $regex?: RegExp | string,
+  $options?: string,
+  $text?: {
+    $search: string,
+    $language?: string,
+    $caseSensitive?: boolean,
+    $diacriticSensitive?: boolean,
+  },
+  $where?: string | ((item: T) => boolean),
+  $all?: T[],
+  $elemMatch?: T extends object ? Query<T> : FieldExpression<T>,
+  $size?: number,
   $bitsAllClear?: any,
   $bitsAllSet?: any,
   $bitsAnyClear?: any,
   $bitsAnySet?: any,
 }
 
-// Utility type to check if a type is an array or object.
-type IsObject<T> = T extends object ? (T extends Array<any> ? false : true) : false
-
 // Recursive type to generate dot-notation keys
-type DotNotationKeys<T> = {
-  [K in keyof T & (string | number)]:
-  T[K] extends Array<infer U>
-  // If it's an array, include both the index and the $ wildcard
-    ? `${K}` | `${K}.$` | `${K}.${DotNotationKeys<U>}`
-  // If it's an object, recurse into it
-    : IsObject<T[K]> extends true
-      ? `${K}` | `${K}.${DotNotationKeys<T[K]>}`
+type DotNotation<T> = {
+  [K in keyof T & string]: T[K] extends Array<infer U>
+    // If it's an array, include both the index and the $ wildcard
+    ? `${K}` | `${K}.$` | `${K}.${DotNotation<U>}`
+    // If it's an object, recurse into it
+    : T[K] extends object
+      ? `${K}` | `${K}.${DotNotation<T[K]>}`
       : `${K}` // Base case: Just return the key
-}[keyof T & (string | number)]
+}[keyof T & string]
 
-type Split<S extends string, Delimiter extends string> =
-  S extends `${infer Head}${Delimiter}${infer Tail}`
-    ? [Head, ...Split<Tail, Delimiter>]
-    : [S]
-
-type GetTypeByParts<T, Parts extends readonly string[]> =
-  Parts extends [infer Head, ...infer Tail]
-    ? Head extends keyof T
-      ? GetTypeByParts<T[Head], Extract<Tail, string[]>>
-      : Head extends '$'
+type GetType<T, P extends string> =
+  P extends `${infer H}.${infer R}`
+    ? H extends keyof T
+      ? T[H] extends Array<infer U>
+        ? GetType<U, R>
+        : GetType<T[H], R>
+      : H extends '$'
         ? T extends Array<infer U>
-          ? GetTypeByParts<U, Extract<Tail, string[]>>
+          ? GetType<U, R>
           : never
         : never
-    : T
-
-type Get<T, Path extends string> =
-  GetTypeByParts<T, Split<Path, '.'>>
-
-type Flatten<T> = T extends any[] ? T[0] : T
+    : P extends keyof T
+      ? T[P]
+      : never
 
 type FlatQuery<T> = {
-  [P in DotNotationKeys<T>]?: Flatten<Get<T, P>> | RegExp | FieldExpression<Flatten<Get<T, P>>>
-}
-type Query<T> = FlatQuery<T> & {
-  $or?: Query<T>[] | undefined,
-  $and?: Query<T>[] | undefined,
-  $nor?: Query<T>[] | undefined,
+  [P in DotNotation<T>]?: FlatQueryValue<T, P>
 }
 
-export type FlatSelector<T extends Record<string, any> = Record<string, any>> = FlatQuery<T> & {
+type FieldValue<U> = U extends string
+  ? string | RegExp | FieldExpression<string>
+  : U | FieldExpression<U>
+
+type FlatQueryValue<T, P extends string> = GetType<T, P> extends never
+  ? never
+  : GetType<T, P> extends Array<infer U>
+    ? FieldValue<U> | FieldValue<U[]>
+    : FieldValue<GetType<T, P>>
+
+type Query<T> = FlatQuery<T> & {
+  $or?: Query<T>[],
+  $and?: Query<T>[],
+  $nor?: Query<T>[],
+}
+
+export type FlatSelector<T extends Record<string, any>> = FlatQuery<T> & {
   $or?: never,
   $and?: never,
   $nor?: never,
 }
-
-type Selector<T extends Record<string, any> = Record<string, any>> = Query<T>
+type Selector<T extends Record<string, any>> = Query<T>
 export default Selector
