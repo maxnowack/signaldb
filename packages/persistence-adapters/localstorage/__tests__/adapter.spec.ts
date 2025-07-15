e19cb6bfc20f68e3e573c0be80435efff856efa1
Max Nowack <maxnowack@users.noreply.github.com>
2025-07-15 10:21:15 +0200

    fix: type and linting errors

--- packages/persistence-adapters/localstorage/__tests__/adapter.spec.ts
+++ packages/persistence-adapters/localstorage/__tests__/adapter.spec.ts
@@ -44,7 +44,7 @@ describe('Persistence', () => {
       await persistence.save([], { added: [], removed: [], modified: [] })
       const collection = new Collection({ persistence })
       await waitForEvent(collection, 'persistence.init')
-      collection.insert({ id: '1', name: 'John' })
+      await collection.insert({ id: '1', name: 'John' })
       await waitForEvent(collection, 'persistence.transmitted')
       const items = collection.find().fetch()
       expect(items).toEqual([{ id: '1', name: 'John' }])
@@ -58,7 +58,7 @@ describe('Persistence', () => {
       const collection = new Collection({ persistence })
       await waitForEvent(collection, 'persistence.init')
 
-      collection.removeOne({ id: '1' })
+      await collection.removeOne({ id: '1' })
       await waitForEvent(collection, 'persistence.transmitted')
 
       const items = collection.find().fetch()
@@ -73,7 +73,7 @@ describe('Persistence', () => {
       const collection = new Collection({ persistence })
       await waitForEvent(collection, 'persistence.init')
 
-      collection.updateOne({ id: '1' }, { $set: { name: 'Johnny' } })
+      await collection.updateOne({ id: '1' }, { $set: { name: 'Johnny' } })
       await waitForEvent(collection, 'persistence.transmitted')
 
       const items = collection.find().fetch()
@@ -89,7 +89,7 @@ describe('Persistence', () => {
       const collection = new Collection({ persistence })
       await waitForEvent(collection, 'persistence.init')
 
-      collection.insert({ id: '2', name: 'Jane' })
+      await collection.insert({ id: '2', name: 'Jane' })
       await waitForEvent(collection, 'persistence.transmitted')
 
       expect(originalItems).toEqual([{ id: '1', name: 'John' }])
@@ -101,11 +101,11 @@ describe('Persistence', () => {
       const collection = new Collection({ persistence })
       await waitForEvent(collection, 'persistence.init')
 
-      collection.insert({ id: '1', name: 'John' })
+      await collection.insert({ id: '1', name: 'John' })
       await waitForEvent(collection, 'persistence.transmitted')
-      collection.insert({ id: '2', name: 'Jane' })
+      await collection.insert({ id: '2', name: 'Jane' })
       await waitForEvent(collection, 'persistence.transmitted')
-      collection.removeOne({ id: '1' })
+      await collection.removeOne({ id: '1' })
       await waitForEvent(collection, 'persistence.transmitted')
 
       const items = collection.find().fetch()
@@ -118,10 +118,10 @@ describe('Persistence', () => {
       const persistence = createLocalStorageAdapter(`test-${Math.floor(Math.random() * 1e17).toString(16)}`)
       await persistence.save([], { added: [], removed: [], modified: [] })
       const collection = new Collection({ persistence })
-      collection.insert({ id: '1', name: 'John' })
-      collection.insert({ id: '2', name: 'Jane' })
-      collection.updateOne({ id: '1' }, { $set: { name: 'Johnny' } })
-      collection.removeOne({ id: '2' })
+      await collection.insert({ id: '1', name: 'John' })
+      await collection.insert({ id: '2', name: 'Jane' })
+      await collection.updateOne({ id: '1' }, { $set: { name: 'Johnny' } })
+      await collection.removeOne({ id: '2' })
       await waitForEvent(collection, 'persistence.init')
 
       const items = collection.find().fetch()
