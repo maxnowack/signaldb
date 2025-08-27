fe7d9145185013e6a6634473d104f359371b4b44
Max Nowack <maxnowack@users.noreply.github.com>
2025-08-27 20:48:51 +0200

    BREAKING CHANGE: refactor StorageAdapter api

--- packages/base/core/src/types/StorageAdapter.ts
+++ packages/base/core/src/types/StorageAdapter.ts
@@ -4,17 +4,23 @@ export interface Changeset<T> {
   removed: T[],
 }
 
-export type LoadResponse<T> = {
-  items: T[],
-  changes?: never,
-} | {
-  changes: Changeset<T>,
-  items?: never,
-}
-
 export default interface StorageAdapter<T extends { id: I } & Record<string, any>, I> {
-  load(): Promise<LoadResponse<T>>,
-  save(items: T[], changes: Changeset<T>): Promise<void>,
-  register(onChange: (data?: LoadResponse<T>) => void | Promise<void>): Promise<void>,
-  unregister?(): Promise<void>,
+  // lifecycle methods
+  setup(): Promise<void>,
+  teardown(): Promise<void>,
+
+  // data retrieval methods
+  readAll(): Promise<T[]>,
+  readPositions(positions: number[]): Promise<T[]>,
+
+  // index methods
+  createIndex(field: string): Promise<void>,
+  dropIndex(field: string): Promise<void>,
+  readIndex(field: string): Promise<Map<any, Set<number>>>,
+
+  // data manipulation methods
+  insert(items: T[]): Promise<void>,
+  replace(items: T[]): Promise<void>,
+  remove(items: T[]): Promise<void>,
+  removeAll(): Promise<void>,
 }
