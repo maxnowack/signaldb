f9793c6cfcb86f96c3b97130f36c967379b0847d
Max Nowack <maxnowack@users.noreply.github.com>
2025-08-29 11:38:08 +0200

    refactor: move index creation related files to root of core package

--- packages/base/core/src/Collection/index.ts
+++ packages/base/core/src/Collection/index.ts
@@ -17,7 +17,7 @@ import type { BaseItem, FindOptions, Transform } from './types'
 export type { BaseItem, Transform, SortSpecifier, FieldSpecifier, FindOptions } from './types'
 export type { CursorOptions } from './Cursor'
 export type { ObserveCallbacks } from './Observer'
-export { default as createIndex } from './createIndex'
+export { default as createIndex } from '../createIndex'
 
 export interface CollectionOptions<T extends BaseItem<I>, I, U = T> {
   /**
